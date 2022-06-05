f28f1903473ba6088e5f1cca5b4295b060f6bd7a
George Duarte <contato@georgeduarte.com.br>
2022-06-05 14:18:57 -0300

    Finalizando escrita das entidades Produto e Categoria, usando Entity e Aggregate

---
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Categoria : Entity
+    {
+        //public Guid Id { get; set; } Herdando de Entity
+        public string Nome { get; private set; }
+        public int Codigo { get; private set; }
+
+        public Categoria(string nome,
+                         int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+    }
+}
