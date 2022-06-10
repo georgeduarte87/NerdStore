6c5c5ca30ba88a9cdf13c4861cf642dac80c89b7
George Duarte <contato@georgeduarte.com.br>
2022-06-10 15:57:27 -0300

    Implementando Controller e View de Carrinho e integrando com Command

---
+++ src/NerdStore.WebApp.MVC/Controllers/ControllerBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace NerdStore.WebApp.MVC.Controllers
+{
+    public abstract class ControllerBase : Controller
+    {
+        // Para simular que existe um cliente logado por enquanto
+        protected Guid ClienteId = Guid.Parse("7ABC988A-2A69-41D6-89A6-93A6B478C500");
+    }
+}
