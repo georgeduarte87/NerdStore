2bed6f90f1d58766a88bb64da892d3eee217c72f
George Duarte <contato@georgeduarte.com.br>
2022-06-11 17:07:55 -0300

    Implementando Query Stack e aplicando ao Carrinho no contexto de Vendas

---
+++ src/NerdStore.WebApp.MVC/Extensions/CartViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NerdStore.Vendas.Application.Queries;
+
+
+namespace NerdStore.WebApp.MVC.Extensions
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly IPedidoQueries _pedidoQueries;
+
+        // TODO: Obter cliente logado
+        protected Guid ClienteId = Guid.Parse("7ABC988A-2A69-41D6-89A6-93A6B478C500");
+
+
+        public CartViewComponent(IPedidoQueries pedidoQueries)
+        {
+            _pedidoQueries = pedidoQueries;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var carrinho = await _pedidoQueries.ObterCarrinhoCliente(ClienteId);
+            var itens = carrinho?.Items.Count ?? 0;
+
+            return View(itens);
+        }
+    }
+}
